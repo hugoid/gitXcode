13273a212c3678612db67bf261e8d9985b2d6507
hugoizquierdo <hugo_id2004@hotmail.com>
2014-08-25 20:12:27 +0200

    the third commit

---
+++ git22/git22/HIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HIDAppDelegate.h
+//  git22
+//
+//  Created by Hugo Izquierdo on 8/25/14.
+//  Copyright (c) 2014 Hugo Izquierdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git22/git22/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git22
+//
+//  Created by Hugo Izquierdo on 8/25/14.
+//  Copyright (c) 2014 Hugo Izquierdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HIDAppDelegate class]));
+    }
+}
